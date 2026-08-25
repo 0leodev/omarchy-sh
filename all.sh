991267de9b56bdc6b6c3bdfe1ccbb458c7b54b2b
Leo <leojimdev@gmail.com>
2026-08-25 18:49:06 -0400

    set to version 3.8.5

--- all.sh
+++ all.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 # Move into this script's own folder, so it works no matter where you run it from
 cd "$(dirname "$0")"
 
-./install-packages.sh
-./install-dotfiles.sh
+./install-packages-3.8.5.sh
+./install-dotfiles-3.8.5.sh
 
 # Prompt for reboot, defaulting to Yes, Yep or Yup on Enter
 read -r -p "Reboot now? [Y/n] " prompt
